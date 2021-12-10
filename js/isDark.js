51eacc8bcbd2f9a400f6c0761a0141d12b8d32ae
TurnOffTheTV <66259107+TurnOffTheTV@users.noreply.github.com>
2021-12-10 09:33:04 -0500

    Create isDark.js

---
+++ js/isDark.js
@@ -0,0 +1,9 @@
+var style = document.getElementById("style");
+var favicon = document.getElementById("favicon");
+var isDark;
+
+setInterval(function(){
+  isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  if(isDark){style.src="https://eclipse-5214.github.io/css/dark.css";favicon.href="https://eclipse-5214.github.io/faviconwhite.png"}
+  if(!isDark){style.src="https://eclipse-5214.github.io/css/light.css";favicon.href="https://eclipse-5214.github.io/faviconblack.png"}
+},100);
