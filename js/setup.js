c249811ddf9059e53bf66310e65975bbdc0d5eef
Eclipse-5214 <83417472+Eclipse-5214@users.noreply.github.com>
2021-12-10 11:56:27 -0500

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,5 @@
-function setup() {
-  var privacyBanner = document.querySelectorAll("[data-gg-privacy-banner-anchor]");
+
+setInterval(function(){ var privacyBanner = document.querySelectorAll("[data-gg-privacy-banner-anchor]");
 	for (var i = 0; i < privacyBanner.length; i++) {
 		privacyBanner[i].parentNode.removeChild(privacyBanner[i]);
-	}
-}
+	}); }, 1);
