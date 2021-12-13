5f188724c21f7e623133bafee46abebf7cf078b6
Eclipse-5214 <83417472+Eclipse-5214@users.noreply.github.com>
2021-12-13 14:07:54 -0500

    Update cube.js

--- Cube/cube.js
+++ Cube/cube.js
@@ -94,7 +94,8 @@ function setup(){
 	if(iframe==="true"){
 	createCanvas(280,280);
 	}else{
-	createCanvas(windowHeight,windowHeight);
+	var canvas = createCanvas(windowWidth, windowHeight);
+	canvas.parent('script-holder');
 	}
 	noStroke();
 	textAlign(CENTER, CENTER);
